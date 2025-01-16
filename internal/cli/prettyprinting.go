7a5d178c579dfdf58e90d942c73a2a0914329b78
jspearritt <joe.spearritt@live.com>
2025-01-16 18:17:19 +1000

    Prettyprinting for commands

--- internal/cli/prettyprinting.go
+++ internal/cli/prettyprinting.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/mcoot/crosswordgame-go/internal/apitypes"
+	"strings"
 )
 
 func PrettyPrint(v interface{}) {
@@ -17,6 +18,18 @@ func apiPrettyPrint(v interface{}) bool {
 	case *apitypes.HealthcheckResponse:
 		printHealthcheckResponse(v)
 		return true
+	case *apitypes.CreateGameResponse:
+		printCreateGameResponse(v)
+		return true
+	case *apitypes.GetGameStateResponse:
+		printGetGameStateResponse(v)
+		return true
+	case *apitypes.GetPlayerStateResponse:
+		printGetPlayerStateResponse(v)
+		return true
+	case *apitypes.GetPlayerScoreResponse:
+		printGetPlayerScoreResponse(v)
+		return true
 	case []interface{}:
 		spew.Dump(v)
 		return true
@@ -28,4 +41,53 @@ func printHealthcheckResponse(v *apitypes.HealthcheckResponse) {
 	fmt.Printf(`Server health:
   Start Time: %s
 `, v.StartTime)
+}
+
+func printCreateGameResponse(v *apitypes.CreateGameResponse) {
+	fmt.Printf(`Game created:
+  Game ID: %s
+`, v.GameId)
+}
+
+func printGetGameStateResponse(v *apitypes.GetGameStateResponse) {
+	fmt.Printf(`Game:
+  Player Count: %d
+  Current State: %s
+  Squares Filled: %d
+  Current Announcing Player: %d
+`, v.PlayerCount, v.Status, v.SquaresFilled, v.CurrentAnnouncingPlayer)
+}
+
+func printGetPlayerStateResponse(v *apitypes.GetPlayerStateResponse) {
+	fmt.Printf(`Player:
+  Board:
+`)
+	printPlayerBoard(v.Board, 4)
+}
+
+func printPlayerBoard(board [][]string, indent int) {
+	var sb strings.Builder
+
+	for _, row := range board {
+		sb.WriteString(strings.Repeat(" ", indent))
+		for j, cell := range row {
+			if cell == "" {
+				sb.WriteString("∅")
+			} else {
+				sb.WriteString(cell)
+			}
+			if j < len(row)-1 {
+				sb.WriteString(" ")
+			}
+		}
+		sb.WriteString("\n")
+	}
+
+	fmt.Println(sb.String())
+}
+
+func printGetPlayerScoreResponse(v *apitypes.GetPlayerScoreResponse) {
+	fmt.Printf(`Score:
+  Player Score: %d
+`, v.Score)
 }
