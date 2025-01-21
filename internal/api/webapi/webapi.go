260dbfe9dffa287b80235be87a59345bf5a9d73a
jspearritt <joe.spearritt@live.com>
2025-01-21 11:54:27 +1000

    Initial lobby page

--- internal/api/webapi/webapi.go
+++ internal/api/webapi/webapi.go
@@ -3,6 +3,7 @@ package webapi
 import (
 	"github.com/a-h/templ"
 	"github.com/gorilla/mux"
+	commonutils "github.com/mcoot/crosswordgame-go/internal/api/utils"
 	"github.com/mcoot/crosswordgame-go/internal/api/webapi/template"
 	"github.com/mcoot/crosswordgame-go/internal/api/webapi/utils"
 	"github.com/mcoot/crosswordgame-go/internal/apitypes"
@@ -29,9 +30,32 @@ func (c *CrosswordGameWebAPI) AttachToRouter(router *mux.Router) error {
 	router.NotFoundHandler = router.NewRoute().BuildOnly().Handler(NotFoundHandler()).GetHandler()
 
 	router.Handle("/", redirect.Handler("/index.html")).Methods("GET")
-	router.Handle("/index.html", staticHandler(template.Index())).Methods("GET")
+	router.Handle("/index", staticHandler(template.Index())).Methods("GET")
+	router.HandleFunc("/lobby/{lobbyId}", c.LobbyPage).Methods("GET")
 
 	return nil
+}
+
+func (c *CrosswordGameWebAPI) LobbyPage(w http.ResponseWriter, r *http.Request) {
+	lobbyId := commonutils.GetLobbyIdPathParam(r)
+	lobbyState, err := c.lobbyManager.GetLobbyState(lobbyId)
+	if err != nil {
+		utils.SendError(logging.GetLogger(r.Context()), r, w, err)
+		return
+	}
+
+	gameSpaceItem := template.EmptyGameSpace()
+	if lobbyState.RunningGame != nil {
+		gameSpaceItem = template.Game(lobbyState.RunningGame.GameId)
+	}
+
+	utils.SendResponse(
+		logging.GetLogger(r.Context()),
+		r,
+		w,
+		template.LobbyPage(lobbyId, lobbyState, gameSpaceItem),
+		200,
+	)
 }
 
 func staticHandler(component templ.Component) http.Handler {
